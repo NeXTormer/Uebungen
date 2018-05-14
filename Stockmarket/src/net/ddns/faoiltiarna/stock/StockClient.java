5f7be49a981d707700dd92e6ff34a2a6cc285752
Felix Holz <felix.holz@me.com>
2018-05-14 14:40:32 +0200

    string transmitting

--- Stockmarket/src/net/ddns/faoiltiarna/stock/StockClient.java
+++ Stockmarket/src/net/ddns/faoiltiarna/stock/StockClient.java
@@ -1,31 +1,45 @@
 package net.ddns.faoiltiarna.stock;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.SpringLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class StockClient extends JFrame {
 
 	private JPanel contentPane;
 	
-	private Socket socket;
 	private JTextField tfHostname;
 	private JTextField tfPort;
+	
+	private Socket socket;
+	private InputStream m_InputStream;
+	private OutputStream m_OutputStream;
 
+	private PrintWriter m_PrintWriter;
+	private BufferedReader m_BufferedReader;
+	private JTextField tfInput;
+	private JTextField tfOutput;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -72,9 +86,6 @@ public class StockClient extends JFrame {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
-		JPanel panelMarket = new JPanel();
-		tabbedPane.addTab("Market", null, panelMarket, null);
-		
 		JPanel panelSettings = new JPanel();
 		tabbedPane.addTab("Settings", null, panelSettings, null);
 		SpringLayout sl_panelSettings = new SpringLayout();
@@ -108,6 +119,44 @@ public class StockClient extends JFrame {
 		tfPort.setColumns(10);
 		tfPort.setText(StockConstants.PORT + "");
 		
+		JPanel panelMarket = new JPanel();
+		tabbedPane.addTab("Market", null, panelMarket, null);
+		panelMarket.setLayout(null);
+		
+		JLabel lblNewLabel_2 = new JLabel("Input");
+		lblNewLabel_2.setBounds(10, 11, 46, 14);
+		panelMarket.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Output");
+		lblNewLabel_3.setBounds(10, 56, 46, 14);
+		panelMarket.add(lblNewLabel_3);
+		
+		tfInput = new JTextField();
+		tfInput.setBounds(82, 8, 327, 20);
+		panelMarket.add(tfInput);
+		tfInput.setColumns(10);
+		
+		tfOutput = new JTextField();
+		tfOutput.setBounds(82, 53, 327, 20);
+		panelMarket.add(tfOutput);
+		tfOutput.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				m_PrintWriter.print(tfInput.getText());
+				try {
+					String echo = m_BufferedReader.readLine();
+					tfOutput.setText(echo);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		btnSend.setBounds(320, 84, 89, 23);
+		panelMarket.add(btnSend);
+		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.SOUTH);
 		
@@ -124,6 +173,13 @@ public class StockClient extends JFrame {
 				try {
 					socket = new Socket(tfHostname.getText(), Integer.valueOf(tfPort.getText()));
 					lblConnectionState.setText("Connected");
+					
+			        m_OutputStream = socket.getOutputStream();
+		            m_InputStream = socket.getInputStream();
+		            
+		            m_PrintWriter = new PrintWriter(m_OutputStream);
+		            m_BufferedReader = new BufferedReader(new InputStreamReader(m_InputStream));
+					
 				} catch (NumberFormatException | IOException e) {
 					e.printStackTrace();
 				}
--- Stockmarket/src/net/ddns/faoiltiarna/stock/Stockmarket.java
+++ Stockmarket/src/net/ddns/faoiltiarna/stock/Stockmarket.java
@@ -1,5 +1,10 @@
 package net.ddns.faoiltiarna.stock;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,6 +12,12 @@ public class Stockmarket {
 
     private ServerSocket m_ServerSocket;
     private Socket m_Socket;
+    
+    private OutputStream m_OutputStream;
+    private InputStream m_InputStream;
+    
+    private PrintWriter m_PrintWriter;
+    private BufferedReader m_BufferedReader;
 
     public static void main(String[] args) {
         new Stockmarket();
@@ -23,6 +34,23 @@ public class Stockmarket {
         {
             m_ServerSocket = new ServerSocket(StockConstants.PORT);
             m_Socket = m_ServerSocket.accept();
+            
+            m_OutputStream = m_Socket.getOutputStream();
+            m_InputStream = m_Socket.getInputStream();
+        
+
+            m_PrintWriter = new PrintWriter(m_OutputStream);
+            m_BufferedReader = new BufferedReader(new InputStreamReader(m_InputStream));
+            
+            
+            while(true)
+            {
+            	String input = m_BufferedReader.readLine();
+            	System.out.println(input);
+            	m_PrintWriter.println(input);
+            	m_PrintWriter.flush();
+            	
+            }
         }
         catch (IOException e)
         {
